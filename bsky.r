0f6096228c4a7bc34c56e7e214ff3e4685cd81c5
ab604 <ab604@soton.ac.uk>
2024-09-26 15:32:02 +0100

    update bot

--- bsky.r
+++ bsky.r
@@ -1,6 +1,11 @@
 # Bluesky post script
 library(atrrr)
 
-# Post base
+# Authenticate 
+auth(user = "owid-daily-data.bsky.social",
+     password = Sys.getenv("ATR_PW"),
+     overwrite = TRUE)
+
+# Post today's screenshot
 post("Today's Our World in Data Daily Data Insight.\nMore details at: https://ourworldindata.org/data-insights",
   image = ".github/owid-shot.png")
