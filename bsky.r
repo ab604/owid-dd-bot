38bd4c28c8d04e789033c248b3860660ff64bb99
ab604 <ab604@soton.ac.uk>
2024-09-26 15:26:50 +0100

    update bot

---
+++ bsky.r
@@ -0,0 +1,6 @@
+# Bluesky post script
+library(atrrr)
+
+# Post base
+post("Today's Our World in Data Daily Data Insight.\nMore details at: https://ourworldindata.org/data-insights",
+  image = ".github/owid-shot.png")
