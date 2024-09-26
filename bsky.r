52a4d57de7527c91668fb00f529fb353bf6e641b
ab604 <ab604@soton.ac.uk>
2024-09-26 18:51:19 +0100

    scrape alt-text

--- bsky.r
+++ bsky.r
@@ -1,5 +1,12 @@
 # Bluesky post script
 library(atrrr)
+library(rvest)
+
+# Scrape alt-text 
+alt_text <- "https://ourworldindata.org/data-insights" |> 
+  read_html() |> 
+  html_elements("#most-recent-data-insight > div.data-insight-blocks > figure > picture > img") |> 
+  html_attr("alt")
 
 # Authenticate 
 auth(user = "owid-daily-data.bsky.social",
@@ -9,5 +16,5 @@ auth(user = "owid-daily-data.bsky.social",
 # Post today's screenshot
 post_skeet("Today's Our World in Data Daily Data Insight.\nMore details at: https://ourworldindata.org/data-insights",
    image = "owid-shot.png",
-   image_alt = "Screenshot of today's Our World in Data Daily Data Insight")
+   image_alt = alt_text)
 
