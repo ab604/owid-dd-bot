64cc7decb8e3ee2d83cce733195936c771b11ac2
ab604 <ab604@soton.ac.uk>
2024-09-26 17:04:54 +0100

    add img alt

--- bsky.r
+++ bsky.r
@@ -8,5 +8,6 @@ auth(user = "owid-daily-data.bsky.social",
 
 # Post today's screenshot
 post_skeet("Today's Our World in Data Daily Data Insight.\nMore details at: https://ourworldindata.org/data-insights",
-   image = "owid-shot.png")
+   image = "owid-shot.png",
+   image_alt = "Screenshot of today's Our World in Data Daily Data Insight")
 
