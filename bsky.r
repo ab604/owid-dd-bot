e604a28eb8243d6998e406f519c2af4794e1f6ed
ab604 <ab604@soton.ac.uk>
2024-09-26 16:03:12 +0100

    update scripts

--- bsky.r
+++ bsky.r
@@ -3,11 +3,11 @@ library(atrrr)
 
 # Authenticate 
 auth(user = "owid-daily-data.bsky.social",
-     password = Sys.getenv("ATR_PW"),
+     password = Sys.getenv("OWID_PW"),
      overwrite = TRUE)
 
 # Post today's screenshot
-# post("Today's Our World in Data Daily Data Insight.\nMore details at: https://ourworldindata.org/data-insights",
-#   image = "owid-shot.png")
+post_skeet("Today's Our World in Data Daily Data Insight.\nMore details at: https://ourworldindata.org/data-insights",
+   image = "owid-shot.png")
 
-post("This is a test!")
+#post_skeet("This is yet another test!")
